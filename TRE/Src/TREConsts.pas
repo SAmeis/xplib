4e619b8fa942df7150773e0e2ddd64c68fae45d2
rogerlais@gmail.com <rogerlais@gmail.com@a795039c-5471-11de-83f4-cb8cd70d16f4>
2010-05-06 22:31:32 +0000

    initial insertion

---
+++ TRE/Src/TREConsts.pas
@@ -0,0 +1,43 @@
+{$IFDEF TREConsts.pas}
+    {$DEFINE DEBUG_UNIT}
+{$ENDIF}
+{$I TRELib.inc}
+
+unit TREConsts;
+
+interface
+
+const
+   //Informações inferidas pelo nome do computador 
+   CMPNAME_LOCAL_ZONE = 'Z';
+   CMPNAME_LOCAL_CENTRAL = 'C';
+   CMPNAME_LOCAL_REGIONAL = 'R';
+   CMPNAME_TYPE_LENGHT = 3;  //Refere-se aos tipos abaixo
+   CMPNAME_TYPE_WORKGROUP = 'STD';
+   CMPNAME_TYPE_DOMAIN = 'WKS';
+   CMPNAME_TYPE_DOMAIN_CONTROLLER = 'PDC';
+
+
+
+   CMPNAME_LOCAL_LENGHT : integer = 3;  //Comprimento do identificador da zona no nome do computador
+   CMPNAME_LOCAL_ANY : integer = 0;
+   CMPNAME_LOCAL_ALL : integer = -1;
+   CMPNAME_LOCAL_MIN_VALUE : integer = 1;
+   CMPNAME_LOCAL_MAX_VALUE :  integer = 77;
+
+   CMPNAME_ID_ANY : integer = 0;
+   CMPNAME_ID_ALL : integer = -1;
+   CMPNAME_ID_MIN_VALUE = 1;
+   CMPNAME_ID_MAX_VALUE = 999999; //Lembrar das virtuais do TRE relacionadas com o patrimonio
+
+
+
+type
+   TTREComputerType = ( ctCentralPDC, ctCentralWKS, ctZonePDC, ctZoneWKS, ctTREWKS, ctVirtual, ctAny );
+   TTRENetType = ( ntUnknow, ntWorkGroup, ntDomain );
+   TTREAccessMedium = ( treamNone, treamVSAT, treamDialled, treamFrameRelay, treamAny );
+
+
+implementation
+
+end.
---
+++ TRE/Src/TREUtils.pas
@@ -0,0 +1,190 @@
+{$IFDEF TREUtils}
+    {$DEFINE DEBUG_UNIT}
+{$ENDIF}
+{$I TRELib.inc}
+
+unit TREUtils;
+
+interface
+
+uses
+    SysUtils, Windows, Classes, TREConsts;
+
+
+type
+    ETREException = class(Exception);
+
+type
+    TTREUtils = class
+    public
+        class procedure CheckComputername(const Name : string); overload;
+        class procedure CheckComputername(const Name : string; Local : Integer); overload;
+        class procedure CheckComputername(const Name : string; ComputerType : TTREComputerType); overload;
+        class procedure CheckComputername(const Name : string; Local : Integer; ComputerType : TTREComputerType); overload;
+        class procedure CheckComputername(const Name : string; Local, CompId : Integer; ComputerType : TTREComputerType); overload;
+        class function GetComputerZone(const Computername : string) : Integer;
+        class function GetNetworkType(const ComputerName : string) : TTRENetType;
+    end;
+
+implementation
+
+uses
+    StrHnd, Str_Pas;
+
+{ TTREUtils }
+
+class procedure TTREUtils.CheckComputername(const Name : string; Local : Integer);
+begin
+    CheckComputername(Name, Local, ctAny);
+end;
+
+class procedure TTREUtils.CheckComputername(const Name : string);
+begin
+    CheckComputername(Name, CMPNAME_LOCAL_ALL, ctAny);
+end;
+
+class procedure TTREUtils.CheckComputername(const Name : string; Local, CompId : Integer; ComputerType : TTREComputerType);
+var
+    prefixFound, prefixExpected : char;
+    prefixLen, localInt, IdInt, idBias : Integer;
+    localStr, IdStr : string;
+    typeFound : TTREComputerType;
+begin
+    //Checa o tipo do computador
+    prefixFound := Str_Pas.GetIChar(UpperCase(Name), 1);
+    case ComputerType of
+        ctCentralPDC, ctCentralWKS : begin
+            prefixExpected := CMPNAME_LOCAL_CENTRAL;
+        end;
+        ctZonePDC, ctZoneWKS : begin
+            prefixExpected := CMPNAME_LOCAL_ZONE;
+        end;
+        ctTREWKS : begin
+            prefixExpected := CMPNAME_LOCAL_REGIONAL;
+        end;
+        ctVirtual : begin
+            prefixExpected := CMPNAME_LOCAL_REGIONAL;
+        end;
+        ctAny : begin
+            prefixExpected := prefixFound;
+        end;
+        else begin
+            raise ETREException.Create('Tipo do computador desconhecido');
+        end;
+    end;
+    if (prefixFound <> prefixExpected) then begin
+        raise ETREException.CreateFmt('Tipo de computador encontrado "%s" diverge do desejado "%s".', [prefixFound, prefixExpected
+            ]);
+    end;
+
+    //Calcula o comprimento do prefixo encontrado
+    case prefixFound of
+        CMPNAME_LOCAL_ZONE : begin
+            prefixLen := 3;
+            idBias    := PrefixLen + CMPNAME_LOCAL_LENGHT + CMPNAME_TYPE_LENGHT;
+            if (Pos(CMPNAME_TYPE_DOMAIN_CONTROLLER, Name) > 0) then begin
+                typeFound := ctZonePDC;
+            end else begin
+                typeFound := ctZoneWKS;
+            end;
+        end;
+        CMPNAME_LOCAL_CENTRAL : begin
+            prefixLen := 3;
+            idBias    := PrefixLen + CMPNAME_LOCAL_LENGHT + CMPNAME_TYPE_LENGHT;
+            if (Pos(CMPNAME_TYPE_DOMAIN_CONTROLLER, Name) > 0) then begin
+                typeFound := ctCentralPDC;
+            end else begin
+                typeFound := ctCentralWKS;
+            end;
+        end;
+        CMPNAME_LOCAL_REGIONAL : begin
+            prefixLen := 4;
+            idBias    := PrefixLen + CMPNAME_LOCAL_LENGHT + CMPNAME_TYPE_LENGHT;
+            typeFound := ctTREWKS;
+            { TODO -oroger -cdsg : Para identificar se a máquina é virtual não existe modo seguro }
+        end;
+        else begin
+            raise ETREException.CreateFmt('Impossível validar nome de computador para prefixo/tipo "%s"', [prefixFound]);
+        end;
+    end;
+
+    //Checa o local do computador
+    if ((Local <> CMPNAME_LOCAL_ANY) and (Local <> CMPNAME_LOCAL_ALL)) then begin
+        localStr := Copy(Name, prefixLen, CMPNAME_LOCAL_LENGHT);
+        try
+            localInt := StrToInt(localStr);
+            if (localInt <> Local) then begin
+                raise ETREException.Create('Local/Zona do computador diverge do esperado.');
+            end;
+        except
+            on E : Exception do begin
+                raise ETREException.CreateFmt('Local/Zona do computador "%s" inválido.'#13'%s', [localStr, E.Message]);
+            end;
+        end;
+    end;
+
+    //checa o id do computador
+    IdStr := System.Copy(Name, idBias+1, Length(Name));
+    try
+        IdInt := StrToInt(IdStr);
+        if ((CompId <> CMPNAME_ID_ALL) and (CompId <> CMPNAME_ID_ANY) and (CompId <> IdInt)) then begin
+            raise ETREException.CreateFmt('O identificador da estação esperado "%d" diverge do encontrado "%d".', [CompId, IdInt]
+                );
+        end;
+    except
+        on E : Exception do begin
+            raise ETREException.CreateFmt('A string "%s" não representa um identificador de estação válido.'#13'%s', [IdStr,
+                E.Message]);
+        end;
+    end;
+
+    //tipo do computador
+    if ((ComputerType <> ctAny) and (ComputerType <> typeFound)) then begin
+        raise ETREException.Create('Tipo do computador encontrado diverge do esperado');
+    end;
+
+end;
+
+class procedure TTREUtils.CheckComputername(const Name : string; ComputerType : TTREComputerType);
+begin
+    CheckComputername(Name, CMPNAME_LOCAL_ALL, ComputerType);
+end;
+
+class procedure TTREUtils.CheckComputername(const Name : string; Local : Integer; ComputerType : TTREComputerType);
+begin
+    CheckComputername(Name, Local, CMPNAME_ID_ANY, ComputerType);
+end;
+
+class function TTREUtils.GetComputerZone(const Computername : string) : Integer;
+{{
+Leitura da zona do computador baseado em seu nome
+
+PRE: Computername no formato de nome de computador de zona eleitoral
+
+Revision: 1/6/2008 - roger
+}
+const
+    CMPNAME_LOCAL_POS = 4; { TODO -oroger -clib : Levar para TREConsts }
+var
+    zone : string;
+begin
+    zone   := Copy(Computername, CMPNAME_LOCAL_POS, CMPNAME_LOCAL_LENGHT);
+    Result := StrToInt(zone);
+end;
+
+class function TTREUtils.GetNetworkType(const ComputerName : string) : TTRENetType;
+begin
+    if (Pos(CMPNAME_TYPE_WORKGROUP, ComputerName) > 0) then begin   //Rede workgroup
+        Result := ntWorkGroup;
+    end else begin
+        if ( (Pos(CMPNAME_TYPE_DOMAIN, ComputerName) > 0 ) or (Pos(CMPNAME_TYPE_DOMAIN_CONTROLLER, ComputerName) > 0)) then begin
+            Result := ntDomain;
+        end else begin
+            raise ETREException.CreateFmt('"%s" não representa um nome de computador válido para uma zona eleitoral', [
+              ComputerName]);
+        end;
+    end;
+end;
+
+end.
+
